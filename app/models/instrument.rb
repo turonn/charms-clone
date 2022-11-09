4b6ccec3759858ee0fa1697a4ebe04554c7fe09f
turonn <72614966+turonn@users.noreply.github.com>
2022-11-09 18:15:44 -0500

    rubocop configuration

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,5 +1,5 @@
 class Instrument < ApplicationRecord
-  NULLABLE_ATTRS = %w( case_id make model serial_number year condition notes program_id )
+  NULLABLE_ATTRS = %w[case_id make model serial_number year condition notes program_id].freeze
   before_save :_nil_if_blank
 
   belongs_to :program
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,6 +1,6 @@
 class Program < ApplicationRecord
   has_many :program_teachers
-  has_many :teachers, :through => :program_teachers
+  has_many :teachers, through: :program_teachers
   has_many :instruments, dependent: :destroy
 
   validates_presence_of :name
